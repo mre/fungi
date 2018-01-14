9cfb75866b48a47d962bc1dbcf51d8c435edca4f
Edoardo Rossi <edd.rossi@gmail.com>
2018-01-14 17:54:34 +0100

    Sample ToString

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@ mod guess_game;
 mod types;
 mod fibonacci;
 mod modules;
+mod to_string;
 
 fn main() {
     types::sample();
     let x: u32 = fibonacci::fib(10);
     modules::try();
     println!("fib(10) = {}", x);
+    to_string::sample();
     guess_game::play();
 }
 

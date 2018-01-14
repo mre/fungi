45b0f459a31862493ae3f5576826e9622c22fe67
Edoardo Rossi <edd.rossi@gmail.com>
2018-01-14 16:54:38 +0100

    Add Fibonacci

--- src/fibonacci.rs
+++ src/fibonacci.rs
@@ -1,24 +1,7 @@
-use std::mem;
-
-fn fibonacci_iterative() {
-    let mut prev = 0;
-    let mut curr = 1usize;
-
-    while let Some(n) = curr.checked_add(prev) {
-        prev = curr;
-        curr = n;
-        println!("{}", n);
+pub fn fib(n: u32) -> u32 {
+    match n {
+        0 => 1,
+        1 => 1,
+        _ => fib(n - 1) + fib(n - 2),
     }
-}
-
-fn fibonacci_recursive(mut prev: usize, mut curr: usize) {
-    mem::swap(&mut prev, &mut curr);
-    if let Some(n) = curr.checked_add(prev) {
-        println!("{}", n);
-        fibonacci(prev, n);
-    }
-}
-
-fn main() {
-    fibonacci_recursive(0, 1);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 mod guess_game;
 mod types;
+mod fibonacci;
 
 fn main() {
     types::sample();
+    let x: u32 = fibonacci::fib(10);
+    println!("fib(10) = {}", x);
     guess_game::play();
 }

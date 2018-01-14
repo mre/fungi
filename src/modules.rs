ae5fe594112845671399acfbb15c79e11865afaf
Edoardo Rossi <edd.rossi@gmail.com>
2018-01-14 17:52:40 +0100

    Nested modules

--- src/modules.rs
+++ src/modules.rs
@@ -20,7 +20,32 @@ pub mod another {
 
 use self::another::series::of;
 
+use std::fmt;
+
+enum TrafficLight {
+    Red,
+    Yellow,
+    Green,
+}
+
+impl fmt::Debug for TrafficLight {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use self::TrafficLight::*;
+        match self {
+            &Red => write!(f, "TrafficLight is RED"),
+            &Yellow => write!(f, "TrafficLight is YELLOW"),
+            &Green => write!(f, "TrafficLight is GREEN"),
+        }
+    }
+}
+
+use self::TrafficLight::{Red, Yellow};
+
 pub fn try() {
     a::series::of::nested_modules();
     of::nested_modules();
+    let red = Red;
+    let yellow = Yellow;
+    let green = TrafficLight::Green;
+    println!("{:?}/{:?}/{:?}", red, yellow, green);
 }

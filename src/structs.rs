98c087e180553d944b557510dbc1058aeff9c69f
Edoardo Rossi <edd.rossi@gmail.com>
2018-01-15 21:24:09 +0100

    Proper printing and area

--- src/structs.rs
+++ src/structs.rs
@@ -8,6 +8,12 @@ pub struct User {
     active: bool,
 }
 
+#[derive(Debug)]
+pub struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 pub fn sample() {
     one();
 
@@ -23,7 +29,17 @@ pub fn sample() {
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(rect1)
+        area_confusing(rect1)
+    );
+
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(&rect1)
     );
 }
 
@@ -93,6 +109,10 @@ fn area_naive(width: u32, height: u32) -> u32 {
     width * height
 }
 
-fn area(dimensions: (u32, u32)) -> u32 {
+fn area_confusing(dimensions: (u32, u32)) -> u32 {
     dimensions.0 * dimensions.1
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }

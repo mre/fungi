d769b2f0a48732ce2f81e51f28473055750940d0
Edoardo Rossi <edd.rossi@gmail.com>
2018-01-15 07:20:00 +0100

    Ownership

--- src/ownership.rs
+++ src/ownership.rs
@@ -24,3 +24,29 @@ fn makes_copy(some_integer: i32) {
     // some_integer comes into scope.
     println!("{}", some_integer);
 } // Here, some_integer goes out of scope. Nothing special happens.
+
+fn gives_ownership() -> String {
+    // gives_ownership will move its
+    // return value into the function
+    // that calls it.
+
+    let some_string = String::from("hello"); // some_string comes into scope.
+
+    some_string // some_string is returned and
+                // moves out to the calling
+                // function.
+}
+
+// takes_and_gives_back will take a String and return one.
+fn takes_and_gives_back(a_string: String) -> String {
+    // a_string comes into
+    // scope.
+    a_string // a_string is returned and moves out to the calling function.
+}
+
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len(); // len() returns the length of a String.
+
+    (s, length)
+}
+

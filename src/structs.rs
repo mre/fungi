974a0bbaa16a38c4060828c29c49a45cec8064f2
Edoardo Rossi <edd.rossi@gmail.com>
2018-01-15 21:13:16 +0100

    Address warnings

--- src/structs.rs
+++ src/structs.rs
@@ -1,5 +1,6 @@
 // https://doc.rust-lang.org/stable/book/second-edition/ch05-01-defining-structs.html
 
+#[derive(Debug)]
 pub struct User {
     username: String,
     email: String,
@@ -8,6 +9,25 @@ pub struct User {
 }
 
 pub fn sample() {
+    one();
+
+    let width1 = 30;
+    let height1 = 50;
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area_naive(width1, height1)
+    );
+
+    let rect1 = (30, 50);
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(rect1)
+    );
+}
+
+fn one() {
     let mut user1 = User {
         email: String::from("someone@example.com"),
         username: String::from("someusername123"),
@@ -18,11 +38,27 @@ pub fn sample() {
     user1.email = String::from("anotheremail@example.com");
 
     // struct update syntax.
-    let _user2 = User {
+    user1 = User {
         email: String::from("another@example.com"),
         username: String::from("anotherusername567"),
         ..user1
     };
+
+    println!("User: {:#?}", user1);
+
+    user1 = build_user(
+        String::from("anotheremail@example.com"),
+        String::from("anothername"),
+    );
+
+    println!("User: {:#?}", user1);
+
+    user1 = build_user_field_init_shorthand(
+        String::from("anotheremail@example.com"),
+        String::from("anothername"),
+    );
+
+    println!("User: {:#?}", user1);
 
     // tuple structs
     struct Color(i32, i32, i32);
@@ -35,7 +71,7 @@ pub fn sample() {
     let _unit_like_struct = UnitLikeStruct();
 }
 
-fn build_user(email: String, username: String) -> User {
+pub fn build_user(email: String, username: String) -> User {
     User {
         email: email,
         username: username,
@@ -44,11 +80,19 @@ fn build_user(email: String, username: String) -> User {
     }
 }
 
-fn build_user_field_init_shorthand(email: String, username: String) -> User {
+pub fn build_user_field_init_shorthand(email: String, username: String) -> User {
     User {
         email,
         username,
         active: true,
         sign_in_count: 1,
     }
+}
+
+fn area_naive(width: u32, height: u32) -> u32 {
+    width * height
+}
+
+fn area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }

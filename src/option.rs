889904b9919b87c67ef80685519422a2913d5715
Edoardo Rossi <edd.rossi@gmail.com>
2018-01-16 21:33:50 +0100

    Match control flow

--- src/option.rs
+++ src/option.rs
@@ -253,6 +253,28 @@ fn four() {
     value_in_cents(Coin::Quarter(UsState::Alaska));
 }
 
+fn five() {
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None => None,
+            Some(i) => Some(i + 1),
+        }
+    }
+
+    let five = Some(5);
+    let _six = plus_one(five);
+    let _none = plus_one(None);
+
+    let some_u8_value = 0u8;
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => (),
+    }
+}
+
 pub fn sample() {
     one();
     two();

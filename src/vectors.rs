a565fde012f4aa4e4968ec496c94856aba170ed8
Edoardo Rossi <edd.rossi@gmail.com>
2018-01-17 07:33:24 +0100

    Vector

--- src/vectors.rs
+++ src/vectors.rs
@@ -10,7 +10,29 @@ pub fn sample() {
     v.push(8);
 
     {
+        // It’s more common to create a Vec<T> that has initial values, and Rust
+        // provides the vec! macro for convenience. The macro will create a new
+        // vector that holds the values we give it.
         let _v = vec![1, 2, 3, 4];
         // do stuff with v
     } // <- v goes out of scope and is freed here
+
+    {
+        let v = vec![1, 2, 3, 4, 5];
+
+        let _third: &i32 = &v[2];
+        // returns Option<&T>
+        let _third: Option<&i32> = v.get(2);
+
+        let v = vec![1, 2, 3, 4, 5];
+
+        // the first [] method will cause a panic! because it references a
+        // nonexistent element.
+        // let _does_not_exist = &v[100];
+        let _does_not_exist = v.get(100);
+
+        // When the program has a valid reference, the borrow checker enforces
+        // the ownership and borrowing rules to ensure this reference and any
+        // other references to the contents of the vector remain valid.
+    }
 }

9250240f9dc7924d7241b11bb75367766019bcc9
Edoardo Rossi <edd.rossi@gmail.com>
2018-01-18 07:20:04 +0100

    Strings

--- src/strings.rs
+++ src/strings.rs
@@ -26,4 +26,29 @@ pub fn sample() {
     let s2 = "bar";
     s1.push_str(&s2);
     println!("s2 is {}", s2);
+
+    let mut s = String::from("lo");
+    s.push('l');
+
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2; // Note that s1 has been moved here and can no longer be used
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = s1 + "-" + &s2 + "-" + &s3;
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = format!("{}-{}-{}", s1, s2, s3);
+
+    // This code also sets s to tic-tac-toe. The format! macro works in the same
+    // way as println!, but instead of printing the output to the screen, it
+    // returns a String with the contents. The version of the code using format!
+    // is much easier to read and also doesn’t take ownership of any of its
+    // parameters.
 }

f034031eac3a4b08c96ad9260db0fd4229ecc218
Edoardo Rossi <edd.rossi@gmail.com>
2018-01-20 09:47:34 +0100

    Traits

--- src/traits.rs
+++ src/traits.rs
@@ -16,3 +16,16 @@ impl Summarizable for NewsArticle {
         format!("{}, by {} ({})", self.headline, self.author, self.location)
     }
 }
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summarizable for Tweet {
+    fn summary(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}

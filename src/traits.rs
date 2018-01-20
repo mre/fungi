27e466d598facbd0fffb7b2a883f03f0b0b61850
Edoardo Rossi <edd.rossi@gmail.com>
2018-01-20 09:47:07 +0100

    Traits

--- src/traits.rs
+++ src/traits.rs
@@ -3,3 +3,16 @@
 pub trait Summarizable {
     fn summary(&self) -> String;
 }
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summarizable for NewsArticle {
+    fn summary(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}

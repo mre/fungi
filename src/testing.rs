2732a4aab409ad8718530d6518f4469bca6fee2e
Edoardo Rossi <edd.rossi@gmail.com>
2018-01-21 22:41:42 +0100

    Add testing sample

--- src/testing.rs
+++ src/testing.rs
@@ -1,5 +1,17 @@
 // https://doc.rust-lang.org/stable/book/second-edition/ch11-00-testing.html
 
+#[derive(Debug)]
+pub struct Rectangle {
+    length: u32,
+    width: u32,
+}
+
+impl Rectangle {
+    pub fn can_hold(&self, other: &Rectangle) -> bool {
+        self.length > other.length && self.width > other.width
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
@@ -9,7 +21,23 @@ mod tests {
 
     #[test]
     fn another() {
-        panic!("Make this test fail");
+        // panic!("Make this test fail");
+    }
+
+    use super::*;
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            length: 8,
+            width: 7,
+        };
+        let smaller = Rectangle {
+            length: 5,
+            width: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
     }
 }
 

5cedf77255da3c6dd1355513dda17a416e99231c
Edoardo Rossi <edd.rossi@gmail.com>
2018-01-22 13:35:12 +0100

    Return a Result from Config::new

--- projects/minigrep/src/main.rs
+++ projects/minigrep/src/main.rs
@@ -12,12 +12,18 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    // The 'static lifetime is the entire duration of the program. All string
+    // literals have the 'static lifetime, which we can choose to annotate as
+    // follows:
+    //   let s: &'static str = "I have a static lifetime.";
+    // The text of this string is stored directly in the binary of your program
+    // and the binary of your program is always available. Therefore, the
+    // lifetime of all string literals is 'static.
+    fn new(args: &[String]) -> Result<Config, &'static str> {
         // https://doc.rust-lang.org/std/macro.eprint.html
         // https://doc.rust-lang.org/std/process/fn.exit.html
         if args.len() < 3 {
-            eprint!("Not enough parameters!\n\n");
-            process::exit(1);
+            return Err("not enough arguments");
         }
 
         // let program_name = args[0].clone();
@@ -26,14 +32,17 @@ impl Config {
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprint!("Problem parsing arguments: {}\n\n", err);
+        process::exit(1);
+    });
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 

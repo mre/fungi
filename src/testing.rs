e6209531f3d1e14b61e5a10446e92c4023cfdfee
Edoardo Rossi <edd.rossi@gmail.com>
2018-01-22 07:17:36 +0100

    Add testing sample

--- src/testing.rs
+++ src/testing.rs
@@ -12,6 +12,28 @@ impl Rectangle {
     }
 }
 
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+pub fn greeting(name: &str) -> String {
+    format!("Hello {}!", name)
+}
+
+pub struct Guess {
+    value: u32,
+}
+
+impl Guess {
+    pub fn new(value: u32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess { value }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
@@ -38,6 +60,54 @@ mod tests {
         };
 
         assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            length: 8,
+            width: 7,
+        };
+        let smaller = Rectangle {
+            length: 5,
+            width: 1,
+        };
+
+        assert!(!smaller.can_hold(&larger));
+    }
+
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
+    }
+    // Note that in some languages and test frameworks, the parameters to the
+    // functions that assert two values are equal are called expected and actual
+    // and the order in which we specify the arguments matters. However, in
+    // Rust, they’re called left and right instead, and the order in which we
+    // specify the value we expect and the value that the code under test
+    // produces doesn’t matter. We could write the assertion in this test as
+    // assert_eq!(add_two(2), 4), which would result in a failure message that
+    // says assertion failed: `(left == right)` (left: `5`, right: `4`).
+
+    // Under the surface, the assert_eq! and assert_ne! macros use the operators
+    // == and !=, respectively. When the assertions fail, these macros print
+    // their arguments using debug formatting, which means the values being
+    // compared must implement the PartialEq and Debug traits.
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{}`",
+            result
+        );
+    }
+
+    #[test]
+    #[should_panic]
+    fn greater_than_100() {
+        Guess::new(200);
     }
 }
 

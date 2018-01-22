7d498afece11f4d4636ff99274effb3a268a6878
Edoardo Rossi <edd.rossi@gmail.com>
2018-01-22 22:19:37 +0100

    Cleanup

--- projects/minigrep/src/main.rs
+++ projects/minigrep/src/main.rs
@@ -27,8 +27,6 @@ fn main() {
         eprint!("Problem parsing arguments: {}\n\n", err);
         process::exit(1);
     });
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
 
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);

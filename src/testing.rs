14054831c7093107802f35764624cd3b435dbf57
Edoardo Rossi <edd.rossi@gmail.com>
2018-01-22 07:23:11 +0100

    Add testing sample

--- src/testing.rs
+++ src/testing.rs
@@ -43,8 +43,47 @@ impl Guess {
     }
 }
 
+fn prints_and_returns_10(a: i32) -> i32 {
+    println!("I got the value {}", a);
+    10
+}
+
+// argo test -- --test-threads=4
+// argo test -- --test-threads=1
+// cargo test -- --nocapture
+// cargo test one_hundred
+// cargo test add
+// cargo test -- --ignored
+
 #[cfg(test)]
 mod tests {
+    #[test]
+    fn add_two_and_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn add_three_and_two() {
+        assert_eq!(5, add_two(3));
+    }
+
+    #[test]
+    fn one_hundred() {
+        assert_eq!(102, add_two(100));
+    }
+
+    #[test]
+    fn this_test_will_pass() {
+        let value = prints_and_returns_10(4);
+        assert_eq!(10, value);
+    }
+
+    #[test]
+    fn this_test_will_fail() {
+        let value = prints_and_returns_10(8);
+        assert_eq!(5, value);
+    }
+
     #[test]
     fn exploration() {
         assert_eq!(2 + 2, 4);
@@ -123,6 +162,12 @@ mod tests {
     #[should_panic(expected = "Guess value must be less than or equal to 100")]
     fn greater_than_100_with_message() {
         Guess::new(200);
+    }
+
+    #[test]
+    #[ignore]
+    fn expensive_test() {
+        // code that takes an hour to run
     }
 }
 

8901ef56d1fd94a25579b636a1eb8e46357d0c09
Edoardo Rossi <edd.rossi@gmail.com>
2018-01-26 22:16:07 +0100

    Tree structure with parent (weak)

--- src/smart_pointers.rs
+++ src/smart_pointers.rs
@@ -710,7 +710,7 @@ fn eleven() {
         children: RefCell::new(vec![]),
     });
 
-    let branch = Rc::new(Node {
+    let _branch = Rc::new(Node {
         value: 5,
         children: RefCell::new(vec![Rc::clone(&leaf)]),
     });
@@ -765,6 +765,48 @@ fn eleven() {
     *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
 
     println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+
+    // Visualise strong and weak counts
+
+    let leaf = Rc::new(NodeWithParent {
+        value: 3,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![]),
+    });
+
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf),
+    );
+
+    {
+        let branch = Rc::new(NodeWithParent {
+            value: 5,
+            parent: RefCell::new(Weak::new()),
+            children: RefCell::new(vec![Rc::clone(&leaf)]),
+        });
+        *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+        println!(
+            "branch strong = {}, weak = {}",
+            Rc::strong_count(&branch),
+            Rc::weak_count(&branch),
+        );
+
+        println!(
+            "leaf strong = {}, weak = {}",
+            Rc::strong_count(&leaf),
+            Rc::weak_count(&leaf),
+        );
+    }
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf),
+    );
 }
 
 pub fn sample() {

544517cf4ce6cd4612671510854ce277be006256
Edoardo Rossi <edd.rossi@gmail.com>
2018-01-27 16:28:53 +0100

    Multiple messages

--- src/concurrency.rs
+++ src/concurrency.rs
@@ -98,8 +98,36 @@ fn three() {
     // Err value if there aren't any messages this time.
 }
 
+// Multiple messages
+fn four() {
+    use std::thread;
+    use std::sync::mpsc;
+    use std::time::Duration;
+
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
+}
+
 pub fn sample() {
     one();
     two();
     three();
+    four();
 }

15df70c7ce1302dae41c353ffdb0f20b71acb364
Edoardo Rossi <edd.rossi@gmail.com>
2018-01-29 07:25:33 +0100

    Pattern Syntax: destructuring

--- src/patterns.rs
+++ src/patterns.rs
@@ -145,4 +145,42 @@ pub fn sample() {
         3 => println!("three"),
         _ => println!("anything"),
     }
+
+    // Matching Ranges of values with ...
+    let x = 5;
+
+    match x {
+        1...5 => println!("one through five"),
+        _ => println!("something else"),
+    }
+
+    let x = 'c';
+
+    match x {
+        'a'...'j' => println!("early ASCII letter"),
+        'k'...'z' => println!("late ASCII letter"),
+        _ => println!("something else"),
+    }
+
+    // Destructuring to Break Apart Values
+    struct Point {
+        x: i32,
+        y: i32,
+    }
+
+    let p = Point { x: 0, y: 7 };
+
+    let Point { x: a, y: b } = p;
+    assert_eq!(0, a);
+    assert_eq!(7, b);
+
+    let Point { x, y } = p;
+    assert_eq!(0, x);
+    assert_eq!(7, y);
+
+    match p {
+        Point { x, y: 0 } => println!("On the x axis at {}", x),
+        Point { x: 0, y } => println!("On the y axis at {}", y),
+        Point { x, y } => println!("On neither axis: ({}, {})", x, y),
+    }
 }

f5714b0503e84fcc86b88708f7216a79f2e1e9fe
Edoardo Rossi <edd.rossi@gmail.com>
2018-02-12 21:19:16 +0100

    Project: llistz

--- projects/llistz/src/second.rs
+++ projects/llistz/src/second.rs
@@ -103,6 +103,10 @@ impl<T> List<T> {
         // handle this is with the as_ref method on Option
         self.head.as_ref().map(|node| &node.elem)
     }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| &mut node.elem)
+    }
 }
 
 impl<T> Drop for List<T> {

9a193cd6ee005fff888f5f5e9716218f864fc339
Edoardo Rossi <edd.rossi@gmail.com>
2018-02-13 22:17:27 +0100

    Project: llistz

--- projects/llistz/src/fourth.rs
+++ projects/llistz/src/fourth.rs
@@ -14,6 +14,46 @@ struct Node<T> {
     prev: Link<T>,
 }
 
+impl<T> Node<T> {
+    fn new(elem: T) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(Node {
+            elem: elem,
+            prev: None,
+            next: None,
+        }))
+    }
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List {
+            head: None,
+            tail: None,
+        }
+    }
+
+    // RefCell
+    // https://doc.rust-lang.org/std/cell/struct.RefCell.html
+    // A mutable memory location with dynamically checked borrow rules
+    // See the module-level documentation for more.
+    // https://doc.rust-lang.org/std/cell/index.html
+
+    pub fn push_front(&mut self, elem: T) {
+        let new_head = Node::new(elem);
+        match self.head.take() {
+            Some(old_head) => {
+                old_head.borrow_mut().prev = Some(new_head.clone());
+                new_head.borrow_mut().next = Some(old_head);
+                self.head = Some(new_head);
+            }
+            None => {
+                self.tail = Some(new_head.clone());
+                self.head = Some(new_head);
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::List;

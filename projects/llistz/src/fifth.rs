8704063db64e91e39eab095c5be8b3bd6c6a7e86
Edoardo Rossi <edd.rossi@gmail.com>
2018-02-17 23:11:47 +0100

    Project: llistz

--- projects/llistz/src/fifth.rs
+++ projects/llistz/src/fifth.rs
@@ -46,16 +46,41 @@ pub struct List<'a, T: 'a> {
 
 type Link<T> = Option<Box<Node<T>>>;
 
-struct Node<T> {
-    elem: T,
-    next: Link<T>,
-}
+// struct Node<T> {
+//     elem: T,
+//     next: Link<T>,
+// }
+//
+// type Link<T> = Option<Box<Node<T>>>;
+//
+// struct Node<T> {
+//     elem: T,
+//     next: Link<T>,
+// }
+
+// pub struct List<T> {
+//     head: Link<T>,
+//     tail: *mut Node<T>, // DANGER DANGER
+// }
+
+// https://doc.rust-lang.org/nightly/nomicon/
 
 type Link<T> = Option<Box<Node<T>>>;
 
 struct Node<T> {
     elem: T,
     next: Link<T>,
+}
+
+use std::ptr;
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List {
+            head: None,
+            tail: ptr::null_mut(),
+        }
+    }
 }
 
 impl<'a, T> List<'a, T> {
@@ -145,39 +170,93 @@ impl<'a, T> List<'a, T> {
     //              ^~~~~~~~~~~~
     // help: run `rustc --explain E0106` to see a detailed explanation
 
-    pub fn push(&'a mut self, elem: T) {
-        let new_tail = Box::new(Node {
+    // pub fn push(&'a mut self, elem: T) {
+    //     let new_tail = Box::new(Node {
+    //         elem: elem,
+    //         // When you push onto the tail, your next is always None
+    //         next: None,
+    //     });
+    //     // Put the box in the right place, and then grab a reference to its Node
+    //     let new_tail = match self.tail.take() {
+    //         Some(old_tail) => {
+    //             // If the old tail existed, update it to point to the new tail
+    //             old_tail.next = Some(new_tail);
+    //             old_tail.next.as_mut().map(|node| &mut **node)
+    //         }
+    //         None => {
+    //             // Otherwise, update the head to point to it
+    //             self.head = Some(new_tail);
+    //             self.head.as_mut().map(|node| &mut **node)
+    //         }
+    //     };
+    //     self.tail = new_tail;
+    // }
+
+    pub fn push(&mut self, elem: T) {
+        let mut new_tail = Box::new(Node {
             elem: elem,
-            // When you push onto the tail, your next is always None
             next: None,
         });
 
-        // Put the box in the right place, and then grab a reference to its Node
-        let new_tail = match self.tail.take() {
-            Some(old_tail) => {
-                // If the old tail existed, update it to point to the new tail
-                old_tail.next = Some(new_tail);
-                old_tail.next.as_mut().map(|node| &mut **node)
-            }
-            None => {
-                // Otherwise, update the head to point to it
-                self.head = Some(new_tail);
-                self.head.as_mut().map(|node| &mut **node)
-            }
-        };
+        let raw_tail: *mut _ = &mut *new_tail;
 
-        self.tail = new_tail;
+        // .is_null checks for null, equivalent to checking for None
+        if !self.tail.is_null() {
+            // If the old tail existed, update it to point to the new tail
+            self.tail.next = Some(new_tail);
+        } else {
+            // Otherwise, update the head to point to it
+            self.head = Some(new_tail);
+        }
+
+        self.tail = raw_tail;
+    }
+
+    pub fn pop(&'a mut self) -> Option<T> {
+        // Grab the list's current head
+        self.head.take().map(|head| {
+            let head = *head;
+            self.head = head.next;
+
+            // If we're out of `head`, make sure to set the tail to `None`.
+            if self.head.is_none() {
+                self.tail = None;
+            }
+
+            head.elem
+        })
     }
 }
 
 #[cfg(test)]
 mod test {
-    // use super::List;
-
+    use super::List;
     #[test]
     fn basics() {
-        // let mut list = List::new();
+        let mut list = List::new();
 
-        assert_eq!(1, 1);
+        // Check empty list behaves right
+        assert_eq!(list.pop(), None);
+
+        // Populate list
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push(4);
+        list.push(5);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), None);
     }
 }

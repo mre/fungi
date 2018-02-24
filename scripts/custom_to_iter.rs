bb041104acae5c9dbd2302f992b932694d53b13c
Edoardo Rossi <edd.rossi@gmail.com>
2018-02-24 14:30:37 +0100

    Script: custom to_iter

--- scripts/custom_to_iter.rs
+++ scripts/custom_to_iter.rs
@@ -50,6 +50,7 @@ impl IntoIterator for IterStuff {
         vrng.into_iter()
     }
 }
+
 // rustc scripts/custom_to_iter.rs --out-dir ./target/ && ./target/custom_to_iter
 fn main() {
     println!("Now we can make a new collection...");
@@ -72,3 +73,39 @@ fn main() {
         println!("{:?}", n);
     }
 }
+
+// Trait std::iter::Iterator
+// https://doc.rust-lang.org/std/iter/trait.Iterator.html#method.by_ref
+//
+// fn by_ref(&mut self) -> &mut Self
+// Borrows an iterator, rather than consuming it.
+// This is useful to allow applying iterator adaptors while still retaining
+// ownership of the original iterator.
+//
+// Examples
+// Basic usage:
+//
+// let a = [1, 2, 3];
+// let iter = a.into_iter();
+// let sum: i32 = iter.take(5)
+//                    .fold(0, |acc, &i| acc + i );
+// assert_eq!(sum, 6);
+//
+// if we try to use iter again, it won't work. The following line
+// gives "error: use of moved value: `iter`
+// assert_eq!(iter.next(), None);
+//
+// let's try that again
+// instead, we add in a .by_ref()
+//
+// let a = [1, 2, 3];
+// let mut iter = a.into_iter();
+// let sum: i32 = iter.by_ref()
+//                    .take(2)
+//                    .fold(0, |acc, &i| acc + i );
+// assert_eq!(sum, 3);
+//
+// now this is just fine:
+// assert_eq!(iter.next(), Some(&3));
+// assert_eq!(iter.next(), None);
+//

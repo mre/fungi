ca72a0f4117b2e7fabfaa44e7b8bee22954b2da6
Edoardo Rossi <edd.rossi@gmail.com>
2018-02-27 09:03:22 +0100

    Project: Procedural Macros and custom Derive

--- projects/hello-world-derive/src/lib.rs
+++ projects/hello-world-derive/src/lib.rs
@@ -9,6 +9,56 @@
 //! will allow for all sorts of advanced metaprogramming in Rust, but
 //! today, they're only for custom derive.
 
+// Sources
+// Procedural Macros: https://doc.rust-lang.org/book/first-edition/procedural-macros.html
+// Commenting: https://doc.rust-lang.org/book/second-edition/ch14-02-publishing-to-crates-io.html
+
+#[derive(Debug)]
+struct DPoint {
+    x: i32,
+    y: i32,
+}
+
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+use std::fmt;
+
+impl fmt::Debug for Point {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Point {{ x: {}, y: {} }}", self.x, self.y)
+    }
+}
+
+/// Examples
+///
+/// ```
+/// #[macro_use]
+/// extern crate hello_world_derive;
+///
+/// trait HelloWorld {
+///     fn hello_world();
+/// }
+///
+/// #[derive(HelloWorld)]
+/// struct FrenchToast;
+///
+/// #[derive(HelloWorld)]
+/// struct Waffles;
+///
+/// #[derive(HelloWorld)]
+/// struct Pancakes;
+///
+/// fn main() {
+///     FrenchToast::hello_world();
+///     Waffles::hello_world();
+///     Pancakes::hello_world();
+/// }
+/// ```
+///
+
 #[cfg(test)]
 mod tests {
     #[test]

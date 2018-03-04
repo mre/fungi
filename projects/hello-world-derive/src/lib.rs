db752d7461899dd7266a012ed33d41003b427d87
Edoardo Rossi <edd.rossi@gmail.com>
2018-03-04 12:21:46 +0100

    Project: Procedural Macros and custom Derive

--- projects/hello-world-derive/src/lib.rs
+++ projects/hello-world-derive/src/lib.rs
@@ -1,4 +1,13 @@
-
+//! # Procedural Macros (and custom Derive)
+//!
+//! `hello-world-derive` is an exercise to create a simple prodedural macro to
+//! provide a custom Derive trait.
+//!
+//! Rust includes several traits that you can derive, but it also lets
+//! you define your own. We can accomplish this task through a feature
+//! of Rust called "procedural macros." Eventually, procedural macros
+//! will allow for all sorts of advanced metaprogramming in Rust, but
+//! today, they're only for custom derive.
 
 #[cfg(test)]
 mod tests {

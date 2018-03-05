b558cde723be8e2267f51580a32f9ca08dcb39d2
Edoardo Rossi <edd.rossi@gmail.com>
2018-03-05 22:44:39 +0100

    Project: Procedural Macros and custom Derive

--- projects/hello-world-derive/tests/integration_test.rs
+++ projects/hello-world-derive/tests/integration_test.rs
@@ -1,8 +1,32 @@
 // https://doc.rust-lang.org/book/second-edition/ch11-03-test-organization.html
 
-extern crate hello-world-derive;
+// https://github.com/rust-lang/rfcs/pull/940
+// https://github.com/rust-lang/rfcs/blob/master/text/0940-hyphens-considered-harmful.md
+#[macro_use]
+extern crate hello_world_derive as hwd;
+
+trait HelloWorld {
+    fn hello_world();
+}
+
+#[derive(HelloWorld)]
+#[HelloWorldName = "the best Pancakes"]
+struct FrenchToast;
+
+#[derive(HelloWorld)]
+#[HelloWorldName = "the best Pancakes"]
+struct Waffles;
+
+
+#[derive(HelloWorld)]
+#[HelloWorldName = "the best Pancakes"]
+struct Pancakes;
 
 #[test]
-fn it_works() {
+fn it_works_as_integration_test() {
+    FrenchToast::hello_world();
+    Waffles::hello_world();
+    Pancakes::hello_world();
+    
     assert_eq!(true, true);
 }

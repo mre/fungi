ad8de6a08268096d429afae07edc94cb20e9377d
Edoardo Rossi <edd.rossi@gmail.com>
2018-03-09 12:13:07 +0100

    Project: Procedural Macros and custom Derive

--- projects/hello-world-derive/tests/integration_test.rs
+++ projects/hello-world-derive/tests/integration_test.rs
@@ -2,6 +2,7 @@
 
 // https://github.com/rust-lang/rfcs/pull/940
 // https://github.com/rust-lang/rfcs/blob/master/text/0940-hyphens-considered-harmful.md
+// https://doc.rust-lang.org/book/first-edition/macros.html
 #[macro_use]
 extern crate hello_world_derive as hwd;
 
@@ -9,23 +10,27 @@ trait HelloWorld {
     fn hello_world();
 }
 
-#[derive(HelloWorld)]
-#[WorldName = "the best FT"]
+trait HelloWorldName {
+    fn hello_world_name() -> String;
+}
+
+#[derive(HelloWorld, HelloWorldName)]
+#[Prefix = "the best"]
 struct FrenchToast;
 
-#[derive(HelloWorld)]
-#[WorldName = "the best WF"]
+#[derive(HelloWorld, HelloWorldName)]
+#[Prefix = "the strange"]
 struct Waffles;
 
-#[derive(HelloWorld)]
-#[WorldName = "the best PC"]
+#[derive(HelloWorld, HelloWorldName)]
+#[Prefix = "the curious"]
 struct Pancakes;
 
 #[test]
 fn it_works_as_integration_test() {
-    FrenchToast::hello_world();
-    Waffles::hello_world();
-    Pancakes::hello_world();
+    assert_eq!(FrenchToast::hello_world_name(), "Hello, World! My name is FrenchToast");
+    assert_eq!(Waffles::hello_world_name(), "Hello, World! My name is Waffles");
+    assert_eq!(Pancakes::hello_world_name(), "Hello, World! My name is Pancakes");
     
     assert_eq!(true, true);
 }

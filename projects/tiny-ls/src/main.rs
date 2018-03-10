3230b4e752b14cb8f838058856607778f803df96
Edoardo Rossi <edd.rossi@gmail.com>
2018-03-11 00:32:30 +0100

    Project: tiny-ls, a `ls` clone written in Rust

--- projects/tiny-ls/src/main.rs
+++ projects/tiny-ls/src/main.rs
@@ -13,9 +13,10 @@ use libc::{S_IRGRP, S_IROTH, S_IRUSR, S_IWGRP, S_IWOTH, S_IWUSR, S_IXGRP, S_IXOT
 use structopt::StructOpt;
 use chrono::{DateTime, Local};
 
+// Crate structopt
+// https://docs.rs/structopt/0.2.5/structopt/#how-to-derivestructopt
 #[derive(StructOpt, Debug)]
 struct Opt {
-    /// Output file
     #[structopt(default_value = ".", parse(from_os_str))]
     path: PathBuf,
 }
@@ -27,6 +28,50 @@ fn parse_permissions(mode: u16) -> String {
     [user, group, other].join("")
 }
 
+// For each flag read, write, and execute, it runs a binary & operation
+// on mode. The output is matched exhaustively against all possible
+// permission patterns.
+//
+// A bitwise AND takes two equal-length binary representations and
+// performs the logical AND operation on each pair of the corresponding
+// bits, by multiplying them. Thus, if both bits in the compared
+// position are 1, the bit in the resulting binary representation is 1
+// (1 × 1 = 1); otherwise, the result is 0 (1 × 0 = 0 and 0 × 0 =
+// 0). For example:
+//
+//     0101 (decimal 5)
+// AND 0011 (decimal 3)
+//   = 0001 (decimal 1)
+//
+// The operation may be used to determine whether a particular bit is
+// set (1) or clear (0). For example, given a bit pattern 0011 (decimal
+// 3), to determine whether the second bit is set we use a bitwise AND
+// with a bit pattern containing 1 only in the second bit:
+//
+//     0011 (decimal 3)
+// AND 0010 (decimal 2)
+//   = 0010 (decimal 2)
+//
+// Because the result 0010 is non-zero, we know the second bit in the
+// original pattern was set. This is often called bit masking. (By
+// analogy, the use of masking tape covers, or masks, portions that
+// should not be altered or portions that are not of interest. In this
+// case, the 0 values mask the bits that are not of interest.)
+//
+// The bitwise AND may be used to clear selected bits (or flags) of a
+// register in which each bit represents an individual Boolean
+// state. This technique is an efficient way to store a number of
+// Boolean Values Using As Little Memory As Possible. For example, 0110
+// (decimal 6) can be considered a set of four flags, where the first
+// and fourth flags are clear (0), and the second and third flags are
+// set (1). The second bit may be cleared by using a bitwise AND with
+// the pattern that has a zero only in the second bit:
+//
+//     0110 (decimal 6)
+// AND 1101 (decimal 13)
+//   = 0100 (decimal 4)
+//
+// https://en.wikipedia.org/wiki/Bitwise_operation#AND
 fn triplet(mode: u16, read: u16, write: u16, execute: u16) -> String {
     match (mode & read, mode & write, mode & execute) {
         (0, 0, 0) => "---",
@@ -72,4 +117,42 @@ fn run(dir: &PathBuf) -> Result<(), Box<Error>> {
         }
     }
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{parse_permissions};
+    
+    #[test]
+    fn parse_permissions_works_for_ur() {
+        let expected: String = "r--------".to_owned();
+        let result: String = parse_permissions(0o400);
+
+        assert_eq!(
+            result,
+            expected,
+            "Value expected: `{}` but the result was: `{}`",
+            expected,
+            result
+        );
+    }
+
+    #[test]
+    fn parse_permissions_works_for_ugor() {
+        let expected: String = "r--r--r--".to_owned();
+        let result: String = parse_permissions(0o444);
+
+        assert_eq!(
+            result,
+            expected,
+            "Value expected: `{}` but the result was: `{}`",
+            expected,
+            result
+        );
+    }
+
+    #[test]
+    fn triplet_works() {
+        assert_eq!(true, true);
+    }
 }

5c1d553b0f19c9ace2b7d4543bf61a387ab74001
Edoardo Rossi <edd.rossi@gmail.com>
2018-03-11 20:21:56 +0100

    Script: Dijkstra's algorithm

--- scripts/dijkstra.rs
+++ scripts/dijkstra.rs
@@ -212,7 +212,6 @@ fn shortest_path(adj_list: &Vec<Vec<Edge>>, start: usize, goal: usize) -> Option
             println!("before the continue");
             continue;
         }
-        println!("after the continue");
 
         // For each node we can reach, see if we can find a way with
         // a lower cost going through this node
@@ -236,30 +235,38 @@ fn shortest_path(adj_list: &Vec<Vec<Edge>>, start: usize, goal: usize) -> Option
     None
 }
 
-// rustc scripts/dijkstra.rs --out-dir ./target && ./target/dijkstra
-fn main() {
-    // This is the directed graph we're going to use.
-    // The node numbers correspond to the different states,
-    // and the edge weights symbolize the cost of moving
-    // from one node to another.
-    // Note that the edges are one-way.
-    //
-    //                  7
-    //          +-----------------+
-    //          |                 |
-    //          v   1        2    |  2
-    //          0 -----> 1 -----> 3 ---> 4
-    //          |        ^        ^      ^
-    //          |        | 1      |      |
-    //          |        |        | 3    | 1
-    //          +------> 2 -------+      |
-    //           10      |               |
-    //                   +---------------+
-    //
-    // The graph is represented as an adjacency list where each index,
-    // corresponding to a node value, has a list of outgoing edges.
-    // Chosen for its efficiency.
-    let graph = vec![
+fn from_to(f: usize, t: usize) -> () {
+    let s: String = format!(
+        "\n\n=== Dijkstra shortest path, from {from} to {to}\n",
+        from = f,
+        to = t
+    );
+    print!("{}", s)
+}
+
+// This is the directed graph we're going to use.
+// The node numbers correspond to the different states,
+// and the edge weights symbolize the cost of moving
+// from one node to another.
+// Note that the edges are one-way.
+//
+//                  7
+//          +-----------------+
+//          |                 |
+//          v   1        2    |  2
+//          0 -----> 1 -----> 3 ---> 4
+//          |        ^        ^      ^
+//          |        | 1      |      |
+//          |        |        | 3    | 1
+//          +------> 2 -------+      |
+//           10      |               |
+//                   +---------------+
+//
+// The graph is represented as an adjacency list where each index,
+// corresponding to a node value, has a list of outgoing edges.
+// Chosen for its efficiency.
+fn initial_graph() -> Vec<Vec<Edge>> {
+    vec![
         // Node 0
         vec![Edge { node: 2, cost: 10 }, Edge { node: 1, cost: 1 }],
         // Node 1
@@ -274,21 +281,20 @@ fn main() {
         vec![Edge { node: 0, cost: 7 }, Edge { node: 4, cost: 2 }],
         // Node 4
         vec![],
-    ];
+    ]
+}
 
-    fn from_to(f: usize, t: usize) -> () {
-        let s: String = format!(
-            "=== Dijkstra shortest path, from {from} to {to}\n",
-            from=f,
-            to=t);
-        print!("{}", s)
-    }
-
+// rustc scripts/dijkstra.rs --out-dir ./target && ./target/dijkstra
+fn main() {
+    let graph = initial_graph();
     from_to(0, 1);
     assert_eq!(shortest_path(&graph, 0, 1), Some(1));
-    format!("\n");
+    from_to(0, 3);
     assert_eq!(shortest_path(&graph, 0, 3), Some(3));
+    from_to(3, 0);
     assert_eq!(shortest_path(&graph, 3, 0), Some(7));
+    from_to(0, 4);
     assert_eq!(shortest_path(&graph, 0, 4), Some(5));
+    from_to(4, 0);
     assert_eq!(shortest_path(&graph, 4, 0), None);
 }

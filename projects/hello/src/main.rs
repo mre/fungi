52056b91e6d7330aa2b3251a02d14ae89bb1655c
Edoardo Rossi <edd.rossi@gmail.com>
2018-03-23 09:53:26 +0100

    Project: Echo server

--- projects/hello/src/main.rs
+++ projects/hello/src/main.rs
@@ -1,7 +1,17 @@
+//! # Building a Multithreaded Web Server
+//!
+//! - https://doc.rust-lang.org/book/second-edition/ch20-00-final-project-a-web-server.html
+//! - https://doc.rust-lang.org/book/second-edition/ch20-01-single-threaded.html
+//! - https://doc.rust-lang.org/book/second-edition/ch20-02-slow-requests.html
+
 use std::fs::File;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
+
+// https://github.com/rust-lang-nursery/log
+#[macro_use]
+extern crate log;
 
 // HTTP request format:
 //
@@ -15,8 +25,11 @@ use std::net::TcpStream;
 // headers CRLF
 // message-body
 
+const BASE_URL: &'static str = "127.0.0.1:8080";
+
 fn main() {
-    let r = TcpListener::bind("127.0.0.1:8080");
+    let r = TcpListener::bind(BASE_URL);
+    info!("server binded to {}", BASE_URL);
     let _r = match r {
         Ok(listener) => for stream in listener.incoming() {
             let r_stream = stream;
@@ -38,26 +51,20 @@ fn handle_connection(mut stream: TcpStream) {
     // The String::from_utf8_lossy function takes a &[u8] and produces a String.
     // println!("request: {}", String::from_utf8_lossy(&buffer[..]));
     let get = b"GET / HTTP/1.1\r\n";
-    if buffer.starts_with(get) {
-        let mut file = File::open("hello.html").unwrap();
 
-        let mut contents = String::new();
-        file.read_to_string(&mut contents).unwrap();
-
-        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
-
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
-        let status_line = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
-        let mut file = File::open("404.html").unwrap();
-        let mut contents = String::new();
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
 
-        file.read_to_string(&mut contents).unwrap();
+    let mut file = File::open(filename).unwrap();
+    let mut contents = String::new();
 
-        let response = format!("{}{}", status_line, contents);
+    file.read_to_string(&mut contents).unwrap();
 
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
-    }
+    let response = format!("{}{}", status_line, contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }

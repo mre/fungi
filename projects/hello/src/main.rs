63735b71c0b32279aa9b7c49cd8b13658942da7a
Edoardo Rossi <edd.rossi@gmail.com>
2018-03-24 18:08:20 +0100

    Project: hello server

--- projects/hello/src/main.rs
+++ projects/hello/src/main.rs
@@ -10,8 +10,13 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 
 // https://github.com/rust-lang-nursery/log
+// https://github.com/sebasmagri/env_logger/
+// https://docs.rs/env_logger/*/env_logger/
 #[macro_use]
 extern crate log;
+extern crate env_logger;
+
+use log::Level;
 
 // HTTP request format:
 //
@@ -27,9 +32,17 @@ extern crate log;
 
 const BASE_URL: &'static str = "127.0.0.1:8080";
 
+// RUST_LOG=hello cargo run
 fn main() {
     let r = TcpListener::bind(BASE_URL);
-    info!("server binded to {}", BASE_URL);
+    env_logger::Builder::from_default_env()
+        .default_format_timestamp(true)
+        .init();
+    
+    if log_enabled!(Level::Info) {
+        info!("server binded to {}", BASE_URL);
+    }
+    
     let _r = match r {
         Ok(listener) => for stream in listener.incoming() {
             let r_stream = stream;
@@ -53,12 +66,15 @@ fn handle_connection(mut stream: TcpStream) {
     let get = b"GET / HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+        ("HTTP/1.1 200 OK\r\n\r\n", "templates/hello.html")
     } else {
-        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "templates/404.html")
     };
 
-    let mut file = File::open(filename).unwrap();
+    let mut file = match File::open(filename) {
+        Ok(file) => file,
+        Err(error) => panic!("There was a problem opening the file: {:?}", error)
+    };
     let mut contents = String::new();
 
     file.read_to_string(&mut contents).unwrap();

c2e376392218a2a484e6280f63d15648bfef1f83
Edoardo Rossi <edd.rossi@gmail.com>
2018-03-25 14:29:28 +0200

    Script: futures

--- projects/futurez/src/main.rs
+++ projects/futurez/src/main.rs
@@ -3,13 +3,15 @@ extern crate futures;
 
 use futures::Future;
 use futures::future::FutureResult;
+use futures::future::Join;
 use futures::future::ok;
 use rand::Rng;
 
 // https://docs.rs/crate/futures/0.1.19
 // https://docs.rs/futures/0.2.0-beta/futures/
 
-fn fut_num() -> (FutureResult<u32, ()>, FutureResult<u32, ()>)  {
+// Box<Future<Item=u64, Error=()>>
+fn fut_num() -> Join<FutureResult<u32, ()>, FutureResult<u32, ()>>  {
     // a RNG that is local to the current thread of execution and seeded by the
     // operating system
     let a = ok::<_, ()>(rand::thread_rng().gen_range(0, 10));
@@ -18,7 +20,26 @@ fn fut_num() -> (FutureResult<u32, ()>, FutureResult<u32, ()>)  {
     // infer it.
     a.join(b)
 }
+
 fn main() {
   let n = fut_num();
-  println!("{:?}", n)
+  let r = n.map(|(x, y)| x + y).wait();
+  println!("Waited for two futures: {:?}", r.unwrap())
 }
+
+// extern crate futures;
+// extern crate tokio_core;
+// fn main() {
+//     use tokio_core::reactor::Core;
+//     use futures::future::lazy;
+//     let mut core = Core::new().unwrap();
+//     let handle = core.handle();
+//     let future = lazy(|| {
+//         handle.spawn(lazy(|| {
+//             Ok(()) // Ok(()) implements FromResult
+//         }));
+//         Ok(2)
+//     });
+//     let expected: Result<_, ()> = Ok(2usize);
+//     assert_eq!(core.run(future), expected);
+// }

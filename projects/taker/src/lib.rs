b07445defa320f977c5b6870f50255142c1a44c7
Edoardo Rossi <edd.rossi@gmail.com>
2018-04-04 00:07:23 +0200

    Project: taker

--- projects/taker/src/lib.rs
+++ projects/taker/src/lib.rs
@@ -5,12 +5,14 @@
 extern crate log;
 extern crate env_logger;
 
+use std::io::prelude::*;
 // use std::fmt;
-use std::fs::{self, DirBuilder};
-use std::io;
 use std::env;
+use std::fs::{self, DirBuilder, File};
+use std::io;
 // use std::io::Read;
 // use std::fs::File;
+use std::path::PathBuf;
 
 mod timez;
 
@@ -23,12 +25,51 @@ pub fn run() -> Result<bool, io::Error> {
         Err(_) => "/".to_owned(),
     };
     info!("considering {} as $HOME", home);
-    let mut src = format!("{}/{}/test/foo.txt", home, BASE_URL);
-    let mut dst = format!("{}/{}/test/{}.foo.txt", home, BASE_URL, timez::datetag());    
+    // https://doc.rust-lang.org/std/path/struct.PathBuf.html
+    let mut dir: PathBuf = [&home, BASE_URL].iter().collect();
+    dir.push("test");
+
+    // https://doc.rust-lang.org/std/fs/struct.DirBuilder.html#method.create
+    match DirBuilder::new().recursive(true).create(&dir) {
+        // https://doc.rust-lang.org/std/io/type.Result.html
+        Ok(_) => {
+            info!("directory {:?} created", &dir);
+            let mut f_dir: PathBuf = dir.clone();
+            f_dir.push("foo.txt");
+            let mut file = File::create(f_dir)?;
+            file.write_all(b"Hello, world!")?;
+        }
+        Err(e) => error!("cannot create directory {:?}: {}", &dir, e),
+    };
+
+    assert!(fs::metadata(&dir).unwrap().is_dir());
+
+    // https://doc.rust-lang.org/std/path/struct.Path.html
+    // impl AsRef<Path> for String
+    //   fn as_ref(&self) -> &Path
+    let mut src: PathBuf = [
+        home,
+        BASE_URL.to_owned(),
+        "test".to_owned(),
+        "foo.txt".to_owned(),
+    ].iter()
+        .collect();
+    let mut dst = format!("{}/{}/test/{}.foo.txt", home, BASE_URL, timez::datetag());
     // https://doc.rust-lang.org/std/fs/fn.copy.html
     // https://doc.rust-lang.org/std/fs/struct.File.html
-    info!("copying {} into {}", src, dst);
-    fs::copy(src, dst)?;
+    info!("copying {:?} into {}", &src, &dst);
+    // https://github.com/rust-lang/rfcs/pull/243
+    let r = fs::copy(&src, &dst);
+    match &r {
+        Ok(n_bytes) => {
+            debug!("copied {} bytes from {:?} to {}", n_bytes, &src, &dst);
+        }
+        Err(e) => {
+            error!("cannot copy {:?} into {}: {}", &src, &dst, e);
+        }
+    };
+
+    debug!("result of the copy: {:?}", r);
 
     src = format!("{}/{}/test/", home, BASE_URL);
     dst = format!("{}/{}/tset/", home, BASE_URL);

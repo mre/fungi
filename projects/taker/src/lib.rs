6cbc835bf8f6927dd2b2cf02734a5d31cd19899c
Edoardo Rossi <edoardo.eddie.rossi@icloud.com>
2018-04-08 19:00:51 +0200

    WIP

--- projects/taker/src/lib.rs
+++ projects/taker/src/lib.rs
@@ -10,6 +10,8 @@ use std::io::prelude::*;
 use std::env;
 use std::fs::{self, DirBuilder, DirEntry, File, ReadDir};
 use std::io;
+// use std::io::{Error, ErrorKind};
+
 // use std::io::Read;
 // use std::fs::File;
 use std::path::{Path, PathBuf};
@@ -17,6 +19,48 @@ use std::path::{Path, PathBuf};
 mod timez;
 
 const BASE_URL: &'static str = "Downloads";
+
+// #[derive(Debug)]
+// struct Operation  {
+//     name: String,
+// }
+
+#[derive(Debug)]
+enum Operation {
+    CopyFile,
+    CopyDir,
+}
+
+fn verify_operation(op: Operation, src: &PathBuf, dst: &PathBuf) -> Result<bool, String> {
+    debug!("verifying the result of {:?}", op);
+    let mut s_size: u64 = 0;
+    let mut d_size: u64 = 0;
+    match fs::metadata(src) {
+        Ok(md) => {
+            // md: fs::Metadata
+            s_size = md.len();
+            debug!("meta from the source (size: {:?})", s_size);
+        },
+        Err(e) => {
+            error!("cannot get metadata from {:?}: {}", src, e);
+        },
+    };
+    match fs::metadata(dst) {
+        Ok(md) => {
+            d_size = md.len();
+            debug!("meta from the destination (size: {:?})", d_size);
+        },
+        Err(e) => {
+            error!("cannot get metadata from {:?}: {}", dst, e);
+        },
+    };
+    if s_size != d_size {
+        error!("Something something has been lost performing {:?} on {:?} and {:?}", op, src, dst);
+        return Err(String::from("mismatching_size"));
+    }
+    info!("Operation {:?} on {:?} and {:?} successful", op, src, dst);
+    Ok(true)
+}
 
 // https://doc.rust-lang.org/std/fs/fn.read_dir.html
 // one possible implementation of walking a directory only visiting files
@@ -28,17 +72,17 @@ fn visit_dirs(dir: &Path, cb: &Fn(&DirEntry)) -> io::Result<()> {
         match r_rds {
             Ok(entries) => {
                 for entry in entries {
-                  let entry = entry?;
-                let path = entry.path();
-                if path.is_dir() {
-                    visit_dirs(&path, cb)?;
-                } else {
-                    cb(&entry);
+                    let entry = entry?;
+                    let path = entry.path();
+                    if path.is_dir() {
+                        visit_dirs(&path, cb)?;
+                    } else {
+                        cb(&entry);
+                    }
                 }
-                }
-            },
+            }
             // https://doc.rust-lang.org/std/io/struct.Error.html
-            Err(e) => { error!("cannot visit dir {:?} {}", &dir, e) },
+            Err(e) => error!("cannot visit dir {:?} {}", &dir, e),
         };
     }
     Ok(())
@@ -89,14 +133,22 @@ pub fn run() -> Result<bool, io::Error> {
     let mut r = fs::copy(&src, &dst);
     match &r {
         &Ok(n_bytes) => {
-            debug!("copied {} bytes from {:?} to {:?}", n_bytes, &src, &dst);
+            debug!("OK: copied {} bytes from {:?} to {:?}", n_bytes, &src, &dst);
         }
         &Err(ref e) => {
-            error!("cannot copy {:?} into {:?}: {:?}", &src, &dst, e);
+            error!("ERROR: cannot copy {:?} into {:?}: {:?}", &src, &dst, e);
         }
     };
 
-    debug!("result of the copy: {:?}", r);
+    match verify_operation(Operation::CopyFile, &src, &dst) {
+        Ok(_) => {},
+        // https://doc.rust-lang.org/std/process/fn.exit.html
+        // https://doc.rust-lang.org/std/io/struct.Error.html
+        e => {
+            let custom_error = io::Error::new(io::ErrorKind::Other, e);
+            return Error(custom_error);
+        },
+    };
 
     src = [&home, BASE_URL, "test"].iter().collect();
     dst = [&home, BASE_URL, "tset"].iter().collect();

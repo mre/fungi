36a1e4b528d485c4c0b32b86c6c906b2d111f2e0
Edoardo Rossi <edd.rossi@gmail.com>
2018-04-13 18:50:32 +0200

    Project: taker

--- projects/taker/src/lib.rs
+++ projects/taker/src/lib.rs
@@ -275,6 +275,7 @@ pub fn run(cfg: config::Config) -> Result<bool, io::Error> {
                     debug!("expanded path: {:?}", p);
                     if fs::metadata(&p).unwrap().is_dir() {
                         debug!("{:?} is a directory", p);
+                        
                         info!("copying content of {:?} into {:?}", p, &dst);
                         visit_dirs(p, &|f_src| {
                             debug!("entering {:?} found {:?}", p, f_src.file_name());
@@ -290,14 +291,13 @@ pub fn run(cfg: config::Config) -> Result<bool, io::Error> {
                             return copy_file_in(f_src_s, f_dst);
                         })?;
                     } else {
-                            // let f_dst: PathBuf =
-                            //     tag_name(&home, &dst, &PathBuf::from(p.file_name()));
-                            // debug!("destination filename: {:?}", &f_dst);
-                            // let f_src_s: PathBuf = [p, &f_src.path()].iter().collect();
-                            // debug!("source filename: {:?}", &f_src_s);
-                            // return copy_file_in(f_src_s, f_dst);
-                        
                         debug!("{:?} is a file", p);
+                        // let f_dst: PathBuf =
+                        //     tag_name(&home, &dst, &PathBuf::from(p.file_name()));
+                        // debug!("destination filename: {:?}", &f_dst);
+                        // let f_src_s: PathBuf = [p, &f_src.path()].iter().collect();
+                        // debug!("source filename: {:?}", &f_src_s);
+                        // return copy_file_in(f_src_s, f_dst);
                     }
                 } else {
                     error!("cannot strip (or no need to) $HOME from {:?}", f);

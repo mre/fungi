75b7e4f97b9dc0ce6f00ffd5e1b136c4cc9e6415
Edoardo Rossi <edd.rossi@gmail.com>
2018-04-19 16:21:18 +0200

    Project: taker

--- projects/taker/src/compress/mod.rs
+++ projects/taker/src/compress/mod.rs
@@ -2,10 +2,10 @@ extern crate tar;
 
 // use std::io::prelude::*;
 use self::tar::Builder;
-use std::fs::File;
-use std::path::PathBuf;
-use std::fs::DirEntry;
+use std::fs::metadata;
+use std::fs::{DirEntry, File};
 use std::io;
+use std::path::PathBuf;
 
 use walkers;
 
@@ -13,13 +13,22 @@ pub fn compress(src: &PathBuf, dst: &str) -> io::Result<()> {
     let file = File::create(dst).unwrap();
     let mut a = Builder::new(file);
 
-    return walkers::visit_mut_dirs(src, &mut move |f_src: &DirEntry| {
-        debug!("entering {:?} found {:?}", src, f_src.file_name());
+    return walkers::visit_mut_dirs(src, &mut move |f: &DirEntry| {
+        debug!("entering {:?} found {:?}", src, f.file_name());
 
-        a.append_path("file1.txt").unwrap();
-        a.append_file("file2.txt", &mut File::open("file3.txt").unwrap())
-            .unwrap();
-
+        match metadata(PathBuf::from(f.file_name())) {
+            Ok(meta) => {
+                if meta.is_dir() {
+                    // TODO: fix duplication here (visit should stop the
+                    // traversal.
+                    debug!("{:?} is a directory", f);
+                    a.append_dir_all(f.file_name(), f.file_name()).unwrap();
+                } else {
+                    a.append_path(f.file_name()).unwrap();
+                }
+            }
+            _ => panic!("boom"),
+        }
         return Ok(true);
     });
 }

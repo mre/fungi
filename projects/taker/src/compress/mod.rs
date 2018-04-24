3e7b2e242fba06af7374093c3d5709acc799506d
Edoardo Rossi <edd.rossi@gmail.com>
2018-04-24 13:10:21 +0200

    Project: taker

--- projects/taker/src/compress/mod.rs
+++ projects/taker/src/compress/mod.rs
@@ -3,32 +3,23 @@ extern crate tar;
 // use std::io::prelude::*;
 use self::tar::Builder;
 use std::fs::metadata;
-use std::fs::{DirEntry, File};
+use std::fs::File;
 use std::io;
 use std::path::PathBuf;
-
-use walkers;
 
 pub fn compress(src: &PathBuf, dst: &str) -> io::Result<()> {
     let file = File::create(dst).unwrap();
     let mut a = Builder::new(file);
-
-    return walkers::visit_mut_dirs(src, &mut move |f: &DirEntry| {
-        debug!("entering {:?} found {:?}", src, f.file_name());
-
-        match metadata(PathBuf::from(f.file_name())) {
-            Ok(meta) => {
-                if meta.is_dir() {
-                    // TODO: fix duplication here (visit should stop the
-                    // traversal.
-                    debug!("{:?} is a directory", f);
-                    a.append_dir_all(f.file_name(), f.file_name()).unwrap();
-                } else {
-                    a.append_path(f.file_name()).unwrap();
-                }
+    match metadata(src) {
+        Ok(meta) => {
+            if meta.is_dir() {
+                debug!("compressing {:?} as a directory", src.file_name().unwrap());
+                a.append_dir(src, src).unwrap();
+            } else {
+                a.append_path(src.file_name().unwrap()).unwrap();
             }
-            _ => panic!("boom"),
         }
-        return Ok(true);
-    });
+        _ => panic!("cannot read the path to compress"),
+    }
+    a.finish()
 }

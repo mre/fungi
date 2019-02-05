d49047f9ce0cd4f0269c2c51ce054cbad757aaec
zeroed <edd.rossi@gmail.com>
2019-02-05 06:54:49 +0100

    Exercism: wordy

--- exercism/rust/wordy/src/lib.rs
+++ exercism/rust/wordy/src/lib.rs
@@ -17,32 +17,53 @@ pub fn answer(command: &str) -> Option<i32> {
         .trim_end_matches(QUESTION_MARK);
 
     let components: Vec<&str> = operation.split(' ').collect::<Vec<&str>>();
-    match components.len() {
-        1 => return Some(components[0].parse::<i32>().expect("boom")),
-        3 => {
-            println!("components: {:?}", components);
-            let a: i32 = components[0].parse::<i32>().expect("boom (a)");
-            let b: i32 = components[2].parse::<i32>().expect("boom (b)");
-            let operator: &str = components[1];
-            match operator {
-                "plus" => return Some(a + b),
-                "minus" => return Some(a - b),
-                "multiplied" => return Some(a * b),
-                "divided" => return Some(a / b),
-                _ => return None,
+    let mut n: Vec<i32> = Vec::new();
+    let mut o: Option<fn(i32, i32) -> i32> = None;
+
+    for c in components {
+        if c.parse::<i32>().is_ok() {
+            n.push(c.parse::<i32>().expect("boom on number"));
+            if o.is_some() {
+                let b = n.pop();
+                let a = n.pop();
+                n.push(o.unwrap()(a.unwrap(), b.unwrap()));
+                o = None;
+            }
+            if n.len() > 1 && o.is_none() {
+                return None;
+            }
+        } else {
+            match c {
+                "plus" => {
+                    // TODO: reject multiple operation for all
+                    if o.is_some() || n.len() == 0 {
+                        return None;
+                    } else {
+                        o = Some(|a, b| a + b)
+                    }
+                }
+                "minus" => {
+                    // TODO: reject multiple operation for all
+                    if o.is_some() || n.len() == 0 {
+                        return None;
+                    } else {
+                        o = Some(|a, b| a - b)
+                    }
+                }
+                "multiplied" => o = Some(|a, b| a * b),
+                "divided" => o = Some(|a, b| a / b),
+                "by" => {
+                    // TODO: check if the previous is multiplied or divided
+                    continue;
+                }
+                _ => {
+                    return None;
+                }
             }
         }
-        4 => {
-            println!("components: {:?}", components);
-            let a: i32 = components[0].parse::<i32>().expect("boom (a)");
-            let b: i32 = components[3].parse::<i32>().expect("boom (b)");
-            let operator: &str = components[1];
-            match operator {
-                "multiplied" => return Some(a * b),
-                "divided" => return Some(a / b),
-                _ => return None,
-            }
-        }
-        _ => return None,
     }
+    if o.is_some() {
+        return None;
+    }
+    return n.pop();
 }

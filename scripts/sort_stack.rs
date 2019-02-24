5dd667808c52918cd8b104f3ac9da1d2ead67aa6
zeroed <edd.rossi@gmail.com>
2019-02-24 19:57:52 +0100

    Whitespace

--- scripts/sort_stack.rs
+++ scripts/sort_stack.rs
@@ -2,7 +2,7 @@ fn main() -> () {
     let mut stack: Vec<usize> = vec![9, 3, 2, 4, 1, 5, 4, 3, 8, 7];
     println!("{:?}", stack);
     let mut ordrd: Vec<usize> = Vec::new();
-    
+
     while !stack.is_empty() {
         // pop the next to sort
         let tmp: usize = stack.pop().expect("nothing to pop");

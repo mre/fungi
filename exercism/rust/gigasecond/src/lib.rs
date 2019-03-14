8b503234232a1b7c2912dafcd1d3eb40e4c18321
zeroed <zeroed@posteo.net>
2019-03-14 20:32:24 +0100

    Cleanup

--- exercism/rust/gigasecond/src/lib.rs
+++
@@ -1,7 +0,0 @@
-extern crate chrono;
-use chrono::{DateTime, Duration, Utc};
-
-// Returns a Utc DateTime one billion seconds after start.
-pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    start + Duration::seconds(10_i64.pow(9))
-}
--- exercism/rust/reverse-string/src/lib.rs
+++
@@ -1,7 +0,0 @@
-extern crate unicode_segmentation;
-
-use unicode_segmentation::UnicodeSegmentation;
-
-pub fn reverse(slice: &str) -> String {
-    slice.graphemes(true).rev().collect()
-}
